3406671a9907cd53eeb7aff0c55e9eaef7eee5fe
Jacob Bare <jacob@limit0.io>
2019-12-21 14:30:36 -0600

    Update extracted url rel

--- src/segments/tags.js
+++ src/segments/tags.js
@@ -17,10 +17,14 @@ const updateTags = async () => {
     return map;
   }, new Map());
 
-  const tagsToMigrate = ddtTags.filter(tag => ienNameMap.has(tag.name));
-  const bulkOps = tagsToMigrate.map((tag) => {
+  const bulkOps = ddtTags.map((tag) => {
     // Must do atomic updates to prevent overwriting other migration data.
-    const $set = { 'migrate._id': ienNameMap.get(tag.name) };
+    const ienId = ienNameMap.get(tag.name);
+    const action = ienId ? 'skip' : 'insert';
+    const $set = {
+      'migrate.action': action,
+      ...(ienId && { 'migrate.ienId': ienId }),
+    };
     const updateOne = { filter: { _id: tag._id }, update: { $set } };
     return { updateOne };
   });
@@ -29,6 +33,65 @@ const updateTags = async () => {
   log('DDT migration data set.');
 };
 
+
+let dupeTagPromise;
+/**
+ * Tags that exists in both IEN and DDT.
+ */
+const loadDupeTags = async () => {
+  const projection = { 'migrate.ienId': 1 };
+  if (!dupeTagPromise) {
+    dupeTagPromise = db.collection('ddt', 'tags').find({ 'migrate.ienId': { $exists: true } }, { projection }).toArray();
+  }
+  return dupeTagPromise;
+};
+
+
+let dupeTagMap;
+const getDupeTagMap = async () => {
+  const dupeTags = await loadDupeTags();
+  if (!dupeTagMap) {
+    dupeTagMap = dupeTags.reduce((map, tag) => {
+      map.set(`${tag._id}`, {
+        ddtId: tag._id,
+        ienId: tag.migrate.ienId,
+      });
+      return map;
+    }, new Map());
+  }
+  return dupeTagMap;
+};
+
+const updateExtractedUrls = async () => {
+  log('Updating `tagIds` on extracted URLs...');
+  const tagMap = await getDupeTagMap();
+  const ddtTagIds = [...tagMap.values()].map(({ ddtId }) => ddtId);
+
+  const urlCursor = db.collection('ddt', 'extracted-urls').find({ tagIds: { $in: ddtTagIds } }, {
+    projection: { tagIds: 1 },
+  });
+  const n = await urlCursor.count();
+  log(`Found ${n} URLs to update.`);
+
+  const bulkOps = [];
+  await db.iterateCursor(urlCursor, (url) => {
+    const newTagIds = url.tagIds.map((tagId) => {
+      const ids = tagMap.get(`${tagId}`);
+      if (ids) {
+        return ids.ienId;
+      }
+      return tagId;
+    });
+    const $set = { 'migrate.fields.tagIds': newTagIds };
+    const updateOne = { filter: { _id: url._id }, update: { $set } };
+    bulkOps.push({ updateOne });
+  });
+  log('Writing data...');
+  if (bulkOps.length) await db.collection('ddt', 'extracted-urls').bulkWrite(bulkOps);
+  log('Extracted URL Update complete.');
+};
+
 module.exports = async () => {
   await updateTags();
+  await updateExtractedUrls();
 };
