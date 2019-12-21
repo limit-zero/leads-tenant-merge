8c676e2e67e68145ef4a11af5d4b51fc6cb9982b
Jacob Bare <jacob@limit0.io>
2019-12-21 13:44:25 -0600

    Set migrated tag ids

--- src/segments/tags.js
+++ src/segments/tags.js
@@ -1,7 +1,10 @@
 const db = require('../db');
 
+const { log } = console;
 
-module.exports = async () => {
+const updateTags = async () => {
+  log('Setting migration data to DDT tags...');
+
   const projection = { name: 1 };
 
   const [ienTags, ddtTags] = await Promise.all([
@@ -9,6 +12,23 @@ module.exports = async () => {
     db.collection('ddt', 'tags').find({ deleted: { $ne: true } }, { projection }).toArray(),
   ]);
 
-  console.log(ienTags);
-  console.log(ddtTags);
+  const ienNameMap = ienTags.reduce((map, tag) => {
+    map.set(tag.name, tag._id);
+    return map;
+  }, new Map());
+
+  const tagsToMigrate = ddtTags.filter(tag => ienNameMap.has(tag.name));
+  const bulkOps = tagsToMigrate.map((tag) => {
+    // Must do atomic updates to prevent overwriting other migration data.
+    const $set = { 'migrate._id': ienNameMap.get(tag.name) };
+    const updateOne = { filter: { _id: tag._id }, update: { $set } };
+    return { updateOne };
+  });
+
+  await db.collection('ddt', 'tags').bulkWrite(bulkOps);
+  log('DDT migration data set.');
+};
+
+module.exports = async () => {
+  await updateTags();
 };
