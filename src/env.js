40d52bec3b3d67cec1029b72838efbd5989c600c
Jacob Bare <jacob@limit0.io>
2019-12-22 15:50:42 -0600

    Rework db env values

--- src/env.js
+++ src/env.js
@@ -1,11 +1,21 @@
 const {
   cleanEnv,
   validators,
+  str,
 } = require('@base-cms/env');
 
 const { nonemptystr } = validators;
 
 module.exports = cleanEnv(process.env, {
-  MONGO_DSN_DDT: nonemptystr({ desc: 'The MongoDB DSN to connect to for DDT.' }),
-  MONGO_DSN_IEN: nonemptystr({ desc: 'The MongoDB DSN to connect to for IEN.' }),
+  DB_HOST: nonemptystr(),
+
+  IEN_DB_USER: nonemptystr(),
+  IEN_DB_PASS: nonemptystr(),
+  IEN_DB_AUTH: str(),
+  IEN_DB_NAME: nonemptystr(),
+
+  DDT_DB_USER: nonemptystr(),
+  DDT_DB_PASS: nonemptystr(),
+  DDT_DB_AUTH: str(),
+  DDT_DB_NAME: nonemptystr(),
 });
